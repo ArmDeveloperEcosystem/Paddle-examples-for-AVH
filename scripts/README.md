5a641909fd07f2d2b46a0721fee360123dd4d567
zheng_bicheng <zheng_bicheng@outlook.com>
2023-07-30 19:15:39 +0800

    update for arm avh

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+```bash
+sudo apt install -y build-essential cmake git libgl1 libglib2.0-0
+bash config.sh
+```
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
 bash $script_dir/config_python.sh
