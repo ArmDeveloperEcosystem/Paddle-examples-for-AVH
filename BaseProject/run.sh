a69c065a0fcc3bc8836063edb66d1afdec2347d1
Ubuntu <ubuntu@ip-172-31-82-195.ec2.internal>
2023-12-20 10:49:49 +0000

    update base project

---
+++ BaseProject/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+csolution list packs -s base_project.csolution.yml -m > packs.txt
+cpackget update-index
+cpackget add -f packs.txt
+cbuild base_project.csolution.yml
+VHT_MPS3_Corstone_SSE-300 -C cpu0.CFGDTCMSZ=15 \
+                        -C cpu0.CFGITCMSZ=15 \
+                        -C mps3_board.uart0.out_file=\"-\" \
+                        -C mps3_board.uart0.shutdown_tag=\"EXITTHESIM\" \
+                        -C mps3_board.visualisation.disable-visualisation=1 \
+                        -C mps3_board.telnetterminal0.start_telnet=0 \
+                        -C mps3_board.telnetterminal1.start_telnet=0 \
+                        -C mps3_board.telnetterminal2.start_telnet=0 \
+                        -C mps3_board.telnetterminal5.start_telnet=0 \
+                        out/base_project/V2M-MPS3-SSE-300-FVP/Target_1/base_project.axf \
+                        --stat
