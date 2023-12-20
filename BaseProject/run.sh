042d8989845070b603bb90d12bad442302696521
Ubuntu <ubuntu@ip-172-31-82-195.ec2.internal>
2023-12-20 10:51:36 +0000

    update base project

--- BaseProject/run.sh
+++ BaseProject/run.sh
@@ -13,5 +13,11 @@ VHT_MPS3_Corstone_SSE-300 -C cpu0.CFGDTCMSZ=15 \
                         -C mps3_board.telnetterminal1.start_telnet=0 \
                         -C mps3_board.telnetterminal2.start_telnet=0 \
                         -C mps3_board.telnetterminal5.start_telnet=0 \
-                        out/base_project/V2M-MPS3-SSE-300-FVP/Target_1/base_project.axf \
+                        out/base_project/Target_1/base_project.axf \
                         --stat
+rm -rf out
+rm -rf tmp
+rm -rf base_project.cbuild-idx.yml
+rm -rf base_project.cbuild-pack.yml
+rm -rf base_project+Target_1.cbuild.yml
+rm -rf base_project+Target_1.cprj
