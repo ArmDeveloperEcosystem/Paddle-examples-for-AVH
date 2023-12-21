ae1518028cb9a16921c069a3ae3ce15114888cc8
Zheng-Bicheng <zheng_bicheng@outlook.com>
2023-12-21 17:28:18 +0800

    update cmsis-pack

---
+++ object_classification/main.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <crt_config.h>
+#include <tvm_runtime.h>
+#include <tvmgen_cls.h>
+
+#include "inputs.h"
+#include "outputs.h"
+#include "stdout_USART.h"
+
+#if defined (__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
+__asm("  .global __ARM_use_no_argv\n");
+#endif
+
+int main(){
+	stdout_init();
+#if USE_MOBILENETV3
+	struct tvmgen_cls_inputs cls_inputs = {
+			.x = input,
+	};
+#endif
+	struct tvmgen_cls_outputs cls_outputs = {
+			.output = output,
+	};
+		
+	tvmgen_cls_run(&cls_inputs, &cls_outputs);
+	int index = 0;
+	for(int i = 0; i < output_len;i++){
+		if(output[i] > output[index]){
+				index = i;
+		}
+	}
+  printf("%d,%f\n", index, output[index]);
+	printf("EXITTHESIM\r\n");
+}
