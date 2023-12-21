a137c9b7f026e78b1a7b3d3bba16e73b27f18e76
Zheng-Bicheng <zheng_bicheng@outlook.com>
2023-12-21 23:42:33 +0800

    update cmsis-pack

--- object_detection/run_demo.sh
+++ object_detection/run_demo.sh
@@ -56,9 +56,11 @@ echo "Device name is $DEVICE"
 if [ "$DEVICE" == "cortex-m55" ]; then
    RUN_DEVICE_NAME="M55"
    VHT_Platform="VHT_MPS3_Corstone_SSE-300"
+   TVM_TARGET="cortex-m55"
 elif [ "$DEVICE" == "cortex-m85" ]; then
    RUN_DEVICE_NAME="M85"
    VHT_Platform="VHT_Corstone_SSE-310"
+   TVM_TARGET="cortex-m85"
 else
   echo 'ERROR: --device only support cortex-m55/cortex-m85' >&2
   exit 1
@@ -69,6 +71,8 @@ echo "Model name is $MODEL_NAME"
 if [ "$MODEL_NAME" == "Picodet" ]; then
   wget https://bj.bcebos.com/v1/paddledet/deploy/Inference/picodet_s_320_coco_lcnet_no_nms.tar
   tar -xf picodet_s_320_coco_lcnet_no_nms.tar
+  rm -rf picodet_s_320_coco_lcnet_no_nms.tar
+  mv picodet_s_320_coco_lcnet_no_nms model
 else
   echo 'ERROR: --model_name only support Picodet' >&2
   exit 1
@@ -76,15 +80,15 @@ fi
 
 # convert paddle model to onnx model
 paddle2onnx --model_dir  "${PWD}/model" \
-            --model_filename inference.pdmodel \
-            --params_filename inference.pdiparams \
-            --save_file inference.onnx
+            --model_filename model.pdmodel \
+            --params_filename model.pdiparams \
+            --save_file model.onnx
 rm -rf "${PWD}/model"
 
 # convert onnx model to tvm model
 python3 -m tvm.driver.tvmc compile --target=cmsis-nn,c \
-    --target-cmsis-nn-mcpu=cortex-m55 \
-    --target-c-mcpu=cortex-m55 \
+    --target-cmsis-nn-mcpu=$TVM_TARGET \
+    --target-c-mcpu=$TVM_TARGET \
     --runtime=crt \
     --executor=aot \
     --executor-aot-interface-api=c \
@@ -92,52 +96,49 @@ python3 -m tvm.driver.tvmc compile --target=cmsis-nn,c \
     --pass-config tir.usmp.enable=1 \
     --pass-config tir.usmp.algorithm=hill_climb \
     --pass-config tir.disable_storage_rewrite=1 \
-    --pass-config tir.disable_vectorize=1 \
-    inference.onnx \
+    --pass-config tir.disable_vectorize=1 model.onnx \
     --output-format=mlf \
     --model-format=onnx \
-    --input-shapes x:[1,3,224,224] \
-    --module-name=cls \
-    --output=cls.tar
-rm inference.onnx
+    --module-name=det \
+    --input-shapes image:[1,3,320,320] \
+    --output=det.tar
+rm model.onnx
 
 # decompression cls.tar
-mkdir -p "${PWD}/cls"
-tar -xvf cls.tar -C "${PWD}/cls"
-rm cls.tar
+mkdir -p "${PWD}/det"
+tar -xvf det.tar -C "${PWD}/det"
+rm det.tar
 
 # create input and output head file
-python3 ./convert_labels.py ./labels/labels.txt
-python3 ./convert_image.py ./imgs/ILSVRC2012_val_00020010.jpg
+ python3 ./convert_image.py ./image/000000014439_640x640.jpg
 
 # build
-csolution list packs -s object_classification.csolution.yml -m > packs.txt
-cpackget update-index
-cpackget add -f packs.txt
+ csolution list packs -s object_detection.csolution.yml -m > packs.txt
+ cpackget update-index
+ cpackget add -f packs.txt
 
-PROJECT_FILE_NAME="object_classification+$MODEL_NAME$RUN_DEVICE_NAME.cprj"
-echo "Project file name is $PROJECT_FILE_NAME"
-cbuild "$PROJECT_FILE_NAME"
+ PROJECT_FILE_NAME="object_detection+$MODEL_NAME$RUN_DEVICE_NAME.cprj"
+ echo "Project file name is $PROJECT_FILE_NAME"
+ cbuild "$PROJECT_FILE_NAME"
 
-rm -rf "${PWD}/cls"
-rm "${PWD}/include/inputs.h"
-rm "${PWD}/include/outputs.h"
-rm "${PWD}/include/labels.h"
+ rm -rf "${PWD}/cls"
+ rm "${PWD}/include/inputs.h"
+ rm "${PWD}/include/outputs.h"
 
-# run
-$VHT_Platform  -C cpu0.CFGDTCMSZ=15 \
-              -C cpu0.CFGITCMSZ=15 \
-              -C mps3_board.uart0.out_file=\"-\" \
-              -C mps3_board.uart0.shutdown_tag=\"EXITTHESIM\" \
-              -C mps3_board.visualisation.disable-visualisation=1 \
-              -C mps3_board.telnetterminal0.start_telnet=0 \
-              -C mps3_board.telnetterminal1.start_telnet=0 \
-              -C mps3_board.telnetterminal2.start_telnet=0 \
-              -C mps3_board.telnetterminal5.start_telnet=0 \
-              "out/object_classification/$MODEL_NAME$RUN_DEVICE_NAME/object_classification.axf" \
-              --stat
+ # run
+ $VHT_Platform  -C cpu0.CFGDTCMSZ=15 \
+               -C cpu0.CFGITCMSZ=15 \
+               -C mps3_board.uart0.out_file=\"-\" \
+               -C mps3_board.uart0.shutdown_tag=\"EXITTHESIM\" \
+               -C mps3_board.visualisation.disable-visualisation=1 \
+               -C mps3_board.telnetterminal0.start_telnet=0 \
+               -C mps3_board.telnetterminal1.start_telnet=0 \
+               -C mps3_board.telnetterminal2.start_telnet=0 \
+               -C mps3_board.telnetterminal5.start_telnet=0 \
+               "out/object_detection/$MODEL_NAME$RUN_DEVICE_NAME/object_detection.axf" \
+               --stat
 
-# clean
-rm -rf out
-rm -rf tmp
-rm -rf packs.txt
+ # clean
+ rm -rf out
+ rm -rf tmp
+ rm -rf packs.txt
