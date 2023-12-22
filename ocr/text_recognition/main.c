00cc8b549de4caaeb570af2412fd5b7562d241ec
Zheng-Bicheng <zheng_bicheng@outlook.com>
2023-12-22 16:02:08 +0800

    update text_recognition

--- ocr/text_recognition/main.c
+++ ocr/text_recognition/main.c
@@ -21,34 +21,56 @@
 #include <stdio.h>
 #include <crt_config.h>
 #include <tvm_runtime.h>
-#include <tvmgen_text_angle_cls.h>
+#include <tvmgen_rec.h>
 #include "inputs.h"
 #include "outputs.h"
 #include "stdout_USART.h"
 
 
 int main(int argc, char** argv) {
+  char dict[]={"#0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~!\"#$%&'()*+,-./  "};
+  int char_dict_nums = 97;
   stdout_init();
-  printf("Starting text angle classification inference\n");
-  struct tvmgen_text_angle_cls_outputs text_angle_cls_outputs = {
-      .output = text_angle_cls_output,
+  printf("Starting ocr rec inference\n");
+  struct tvmgen_rec_outputs rec_outputs = {
+      .output = output,
   };
-  struct tvmgen_text_angle_cls_inputs text_angle_cls_inputs = {
-      .x = text_angle_cls_input,
+  struct tvmgen_rec_inputs rec_inputs = {
+      .x = input,
   };
 
-  tvmgen_text_angle_cls_run(&text_angle_cls_inputs, &text_angle_cls_outputs);
+  tvmgen_rec_run(&rec_inputs, &rec_outputs);
 
   // post process
-  if(text_angle_cls_output[0] >= text_angle_cls_output[1]) {
-    printf("The input picture is a horizontal picture, confidence is %.2f\n",text_angle_cls_output[0]);
-  } else{
-    printf("The input picture is a vertical picture, confidence is %.2f\n", text_angle_cls_output[1]);
+  int char_nums = output_len / char_dict_nums;
+
+  int last_index = 0;
+  float score = 0.f;
+  int count = 0;
+
+  printf("text: ");
+  for (int i = 0; i < char_nums; i++) {
+    int argmax_idx = 0;
+    float max_value = 0.0f;
+    for (int j = 0; j < char_dict_nums; j++){
+      if (output[i * char_dict_nums + j] > max_value){
+        max_value = output[i * char_dict_nums + j];
+        argmax_idx = j;
+      }
+    }
+    if (argmax_idx > 0 && (!(i > 0 && argmax_idx == last_index))) {
+      score += max_value;
+      count += 1;
+      // printf("%d，%f，%c\n", argmax_idx, max_value, dict[argmax_idx]);
+      printf("%c", dict[argmax_idx]);
+    }
+    last_index = argmax_idx;
   }
+  score /= count;
+  printf(", score: %f\n", score);
 
   // The FVP will shut down when it receives "EXITTHESIM" on the UART
   printf("EXITTHESIM\n");
-  while (1 == 1)
-    ;
+  while (1 == 1);
   return 0;
 }
