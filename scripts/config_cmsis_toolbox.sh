92b97e68b355940530b47f2bef9addd99045fddf
Zheng-Bicheng <58363586+Zheng-Bicheng@users.noreply.github.com>
2023-12-25 16:39:53 +0800

    [Hackathon No.88] Deployment verification of PaddleClas model on Arm virtual hardware (#18)
    Update CMSIS Project to the branch.

---
+++ scripts/config_cmsis_toolbox.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download cmsis-toolbox
+cmsis_toolbox_name="cmsis-toolbox-linux-amd64"
+cmsis_toolbox_version="2.2.1"
+cmsis_toolbox_url="https://github.com/Open-CMSIS-Pack/cmsis-toolbox/releases/download/${cmsis_toolbox_version}/${cmsis_toolbox_name}.tar.gz"
+wget ${cmsis_toolbox_url}
+tar -vxf ${cmsis_toolbox_name}.tar.gz
+rm ${cmsis_toolbox_name}.tar.gz
+
+# copy to opt
+mv ${cmsis_toolbox_name} ctools
+rm -rf /opt/ctools
+mv ctools /opt
